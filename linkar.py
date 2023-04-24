0874fff1a41170a1f9455dcbed9113849ba8d01b
CASSIO SANTOS CAIRES <69798464+kasynho@users.noreply.github.com>
2023-04-24 10:17:28 -0300

    Update linkar.py

--- linkar.py
+++ linkar.py
@@ -1,3 +1,46 @@
+# Preciso de um Script no python para linkar versos de texto bíblico.
+# Veja para encontrar as referências nesta string 
+# texto = "Gn 10:15-19; 12:7; 15:7,18-21; 19:10-14,16;20:3—21:7; Ex 2:5;"
+
+# Uso a regex
+#referencias = "re.findall(r'\b((?:[123]\s?)?(?:I*\s?)?[A-ZÀ-Ü][a-zà-ü]+\.?)(\s\d+:)?([\d,-;—\s–:]+\d+)\b', texto)"
+ 
+# O findall retorna dois grupos, o primeiro grupo é abreviação do livro, segundo grupo são as referências;
+ 
+# A linkagem é feita desta funciona desta maneira:
+# Para Gn 12:7
+# <a href='b{livro}.{capitulo}.{versiculo}'>Gn 12:7</a>
+# <a href='b1.12.7'>Gn 12:7</a>
+
+# Para Gn 12:7-15
+# <a href='b{livro}.{capitulo}.{verso_inicio}-{livro}.{capitulo}.{verso_fim}'>Gn 12:7-15</a>
+# <a href='b1.12.7-1.12.15'>Gn 12:7-15</a>
+
+# Para Gn 2:8-10,18 
+# <a href='b{livro}.{capitulo}.{verso_inicio}-{livro}.{capitulo}.{verso_fim}'>Gn 2:8-10</a>, <a href='b{livro}.{capitulo}.{versiculo}'>18</a>
+# <a href='b1.2.8-1.2.10'>Gn 2:8-10</a>,<a href='b1.2.18'>18</a>
+
+# Para Gn 20:3—21:7
+# <a href='b{livro}.{capitulo}.{verso-inicio}-{livro}.{capitulo}.{verso_fim}'>Gn 20:3—21:7</a>
+# <a href='b1.20.3-1.21.7'>Gn 12:7-15</a>
+ 
+# Preciso de um dicionário para converter os livros da bíblia para números:
+# biblia = {'Gn': '1', 'Ex': '2'}
+ 
+# o segundo grupo da regex preciso separar desta maneira:
+# ; separa referencia 
+# : Separa capitulo
+# , separa versiculo
+# - separa versículo início e versículo fim
+# — indica capítulo+inicio a capítulo+fim
+
+# as referências podem ser apresentadas assim:
+# 12:7= capítulo + versículo
+# 15:18-21 = capítulo+versiculo+versiculo inicio+versículo fim
+# 19:10-14,16= capítulo+versiculo inicio+versículo fim+versiculo|capítulo+versiculo
+# 20:3—21:7= — indica capítulo+versículo a capítulo+versiculo
+
+
 import re
 
 biblia = {'Gn': '1', 'Ex': '2'}
